d2046314fb72e0220be26871e0924d80a81fc0d6
Nitin Pal <141559894+nitinpal90@users.noreply.github.com>
2024-06-26 16:23:24 +0530

    Add files via upload

---
+++ Day-3 Python Challenge.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Day-3 Python Challenge 
+
+# # Built-In Function 
+
+# # Absolute Function Denoted by abs()
+
+# In[1]:
+
+
+x = abs(-2.5)
+print(x)
+
+
+# In[2]:
+
+
+y = abs(-5)
+print(y)
+
+
+# In[3]:
+
+
+a = abs(3+5j)
+print(a)
+
+
+# # Binary Function Denoted by bin()
+
+# In[4]:
+
+
+a = bin(10)
+print(a)
+
+
+# In[5]:
+
+
+b = bin(36)
+print(b)
+
+
+# In[6]:
+
+
+c = bin(54)
+print(c)
+
+
+# # Bytes Function Denoted by bytes()
+
+# In[8]:
+
+
+a = bytes(4)
+print(a)
+
+
+# In[9]:
+
+
+b = bytes(8)
+print(b)
+
+
+# # Character Function Denoted by chr()
+
+# In[12]:
+
+
+a = chr(97)
+print(a)
+
+
+# In[13]:
+
+
+b = chr(98)
+print(b)
+
+
+# # Complex Function
+
+# In[14]:
+
+
+a = complex(2,5) ##(Real NUmber, Imaginary Number)
+print(a)
+
+
+# In[15]:
+
+
+b = complex(2.4, 45.6) ## Float Number to Complex Number
+print(b)
+
+
+# In[16]:
+
+
+c = complex('2') ## Given Only Real Number and they Choose Randomly Imaginary Number
+print(c)
+
+
+# # Float Function
+
+# In[17]:
+
+
+a = float(3)
+print(a)
+
+
+# In[18]:
+
+
+b = float(3.5)
+print(b)
+
+
+# # Int Function
+
+# In[19]:
+
+
+a = int(12.5)
+print(a)
+
+
+# # String Function Denoted by str()
+
+# In[21]:
+
+
+a = str(23.76) ## Change the Floating Number to String
+print(a)
+print(type(a))
+
+
+# # Input Function
+
+# In[25]:
+
+
+a = input("Enter Your Name: ")
+print("Hi",a)
+
+
+# In[26]:
+
+
+b = input("Enter Your Age: ")
+print("Your Age is: ",b)
+
+
+# In[27]:
+
+
+a = input("Enter Your Name: ")
+print("Hi",a)
+b = input("Enter Your Age: ")
+print("Your Age is: ",b)
+
